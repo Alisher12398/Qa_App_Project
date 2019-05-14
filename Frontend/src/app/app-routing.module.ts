077a787f27e8d3feebd35282a72e1a2097981712
Alisher <Alisher12398@gmail.com>
2019-05-14 19:06:17 +0600

    V003
    Backend:
    *No changes
    Frontend:
    *Add Angular project
    *Add Providers, Services, Routing, Components

---
+++ Frontend/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BaseComponent } from './base/base.component';
+const routes: Routes = [
+   {path: '', component: BaseComponent},
+  // {path: 'task_lists', component: TaskListsComponent},
+  // {path: 'task_lists/:id', component: TaskListDetailComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ClassProvider, NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BaseComponent } from './base/base.component';
+import { MainComponent } from './main/main.component';
+import { OffersComponent } from './offers/offers.component';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {AuthInterceptor} from './AuthInterceptor';
+import { ProviderService } from './shared/services/provider.service';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuizComponent,
+    ProfileComponent,
+    BaseComponent,
+    MainComponent,
+    OffersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [
+    ProviderService,
+    <ClassProvider> {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

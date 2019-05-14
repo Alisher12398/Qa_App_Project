41b0cedee98b06a13e195d58b50320ef406af715
rinat001 <rinatsabitov7@gmail.com>
2019-05-15 02:19:13 +0600

    FrontBase
    Жить значит унижать

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BaseComponent } from './base/base.component';
+import { MainComponent } from './main/main.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 const routes: Routes = [
-   {path: '', component: BaseComponent},
-  // {path: 'task_lists', component: TaskListsComponent},
-  // {path: 'task_lists/:id', component: TaskListDetailComponent},
+  { path: '', component: MainComponent },
+  { path: 'companies', component: CompaniesComponent },
+  { path: 'registration', component: RegistrationComponent},
+  { path: 'login', component: LoginComponent},
+  
 ];
 
 @NgModule({
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -14,6 +14,15 @@ import {AuthInterceptor} from './AuthInterceptor';
 import { ProviderService } from './shared/services/provider.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CompaniesComponent } from './companies/companies.component';
+import { HeaderComponent } from './header/header.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { AboutComponent } from './about/about.component';
+import { TeamInfoComponent } from './team-info/team-info.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogformComponent } from './logform/logform.component';
+import { RegisterformComponent } from './registerform/registerform.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +32,16 @@ import { CompaniesComponent } from './companies/companies.component';
     BaseComponent,
     MainComponent,
     OffersComponent,
-    CompaniesComponent
+    CompaniesComponent,
+    HeaderComponent,
+    JumbotronComponent,
+    AboutComponent,
+    TeamInfoComponent,
+    FooterComponent,
+    LogformComponent,
+    RegisterformComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,

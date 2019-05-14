4f35cb752b97289ad1c0f9f13319d6fe8e04f17a
Alimur4ikS9 <alinur.sabit.9@gmail.com>
2019-05-15 02:47:59 +0600

    V003
    app,routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BaseComponent } from './base/base.component';
+import { MainComponent } from './main/main.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 const routes: Routes = [
-   {path: '', component: BaseComponent},
-   {path: 'main', component: MainComponent},
-   {path: 'profile', component: ProfileComponent},
+  { path: '', component: MainComponent },
+  { path: 'companies', component: CompaniesComponent },
+  { path: 'registration', component: RegistrationComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'profile', component: ProfileComponent},
 ];
 
 @NgModule({

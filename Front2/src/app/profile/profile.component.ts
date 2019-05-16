bb46d5cbcbeb0c3650bcb89b7f74a065677ae06b
Alisher <Alisher12398@gmail.com>
2019-05-16 15:35:59 +0600

    Alinur: Update Profile

--- Front2/src/app/profile/profile.component.ts
+++ Front2/src/app/profile/profile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
+import { ProviderService } from '../shared/services/provider.service';
+import {Location} from '@angular/common';
+import {IUser} from '../shared/models';
+import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public id_user: number = 0;
+  public users: IUser[] = [];
+  public id = localStorage.key(this.id_user)
+
+  constructor(
+    private provider: ProviderService,
+    private auth: AuthService
+  ) { }
 
   ngOnInit() {
+    if(this.auth.isAuthenticated){
+      this.provider.getUser().then(res => {
+        this.users = res;
+      })
+    }
   }
 
 }

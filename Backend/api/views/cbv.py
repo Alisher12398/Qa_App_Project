629fcd405c6174f9309372e5eefd8faca5795633
Yeldan <yeldan99@gmail.com>
2019-05-16 14:44:24 +0600

    offers purchases

--- Backend/api/views/cbv.py
+++ Backend/api/views/cbv.py
@@ -2,7 +2,7 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from api.models import Qa, Group, OffersPuchases
+from api.models import Qa, Group, OffersPurchases
 from api.serializers import QaSerializer, GroupSerializer, OffersPurchasesSerializer
 from rest_framework.permissions import IsAuthenticated
 
